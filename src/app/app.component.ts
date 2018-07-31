22f3eb64e26f32e413b540208e1875320ee18601
Teresa Juan Blanco <teresa.juanblanco@genomcore.com>
2018-07-31 16:25:40 +0200

    Add routes dinamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+interface Nav {
+    link: string;
+    name: string;
+    exact: boolean;
+}
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
     title = 'Twilio App';
+    nav: Nav[] = [
+        {
+            link: '/',
+            name: 'Home',
+            exact: true
+        },
+        {
+            link: '/oops',
+            name: '404',
+            exact: false
+        }
+    ];
 }
